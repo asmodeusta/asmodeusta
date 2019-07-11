f736763fb42ea0ae3a23cc6d9a47c8931c8872ba
asmodeusta <asmodeusta@gmail.com>
2019-07-11 20:24:10 +0300

    Added: logo

--- usf/modules/site/Controllers/MainController.php
+++ usf/modules/site/Controllers/MainController.php
@@ -12,66 +12,7 @@ class MainController extends Controller
 
     public function actionIndex($page = 1)
     {
-        $symbols = '';
-        $num = 1;
-        if (isset($_POST)
-            && array_key_exists('symbols',$_POST)
-            && array_key_exists('num', $_POST)) {
-            $symbols = $_POST['symbols'];
-            $num = $_POST['num'];
-            $words = $this->generateWords($symbols, $num);
-        }
-        ?>
-        <form method="post">
-            <input type="text" name="symbols" value="<?php echo $symbols; ?>" minlength="2" maxlength="10">
-            <input type="number" name="num" value="<?php echo $num; ?>" min="1" max="10">
-            <input type="submit" value="Generate">
-        </form>
-        <?php
-        if (isset($words)) {
-            echo '<ol>';
-            foreach ($words as $word) {
-                echo '<li>' . $word . '</li>';
-            }
-            echo '</ol>';
-        }
-        $arr1 = ['1' => 1, '2' => 2, '3' => 3];
-        $arr2 = ['1' => 4, '2' => 5, '3' => 6];
-        $arr1 += $arr2;
-        var_dump($arr1);
-    }
-
-    protected function generateWords(string $symbols, int $num)
-    {
-        $symbols = mb_str_split($symbols);
-        $words = [];
-        foreach ($this->generateWord($symbols, $num) as $word) {
-            $words[] = $word;
-        }
-        $result = array_unique($words);
-        sort($result);
-        return $result;
-    }
-
-    protected function generateWord(array $symbols, int $num, $word = '')
-    {
-        foreach ($symbols as $symbol) {
-            $pos = array_search($symbol, $symbols);
-            if (mb_strlen($word)+1 >= $num) {
-                yield $word . $symbol;
-            } else {
-                $newSymbols = $symbols;
-                unset($newSymbols[$pos]);
-                foreach ($this->generateWord(
-                    $newSymbols,
-                    //array_filter($symbols, function ($element) use ($symbol) {return $symbol !== $element; }),
-                    $num,
-                    $word . $symbol
-                ) as $currWord) {
-                    yield $currWord;
-                }
-            }
-        }
+        echo 'This is page #' . $page;
     }
 
     public function actionFact($num=1)
